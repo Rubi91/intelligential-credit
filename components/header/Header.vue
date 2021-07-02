30b01d35cb8111bb57309e704437edc83e29868c
Vega <rubivega91@gmail.com>
2021-07-01 19:31:44 -0500

    Add Header Logo

---
+++ components/header/Header.vue
@@ -0,0 +1,14 @@
+<template>
+<header>
+  <HeaderLogo/>
+</header>
+</template>
+
+<script>
+</script>
+<style lang="scss" scoped>
+header {
+  padding: 10px 25px;
+  background: $charcoal;
+}
+</style>
